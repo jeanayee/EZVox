fe068e49afe103f0115b0c4b086cfdbe90ef0cfc
Jeana Yee <jeana.yee@gmail.com>
2017-01-27 11:57:42 -0500

    create list of songs

---
+++ app/src/main/java/com/jeana/ezvox/Song.java
@@ -0,0 +1,21 @@
+package com.jeana.ezvox;
+
+/**
+ * Created by jeana on 1/27/17.
+ */
+
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
